ba4a8b2b33f6f7592c6006853efb8e210dc2c9b1
Thiago Lucas Soares Brigida <161029335+thiagobrigida@users.noreply.github.com>
2025-05-13 21:44:49 -0300

    Delete gerador-de-senha/geradorsenha-app directory

--- gerador-de-senha/geradorsenha-app/App.tsx
+++
@@ -1,9 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/screens/Home/Home';
-
-export default function App() {
-  return (
-      <Home />
-  );
-}
